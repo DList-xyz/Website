5b7a8e3f74c5c3446a40c078f673ff93e452f124
theADAMJR <adamjf123@gmail.com>
2020-08-15 13:24:50 +0100

    first

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  endpoint: '/api',
-  discordInvite: 'https://discord.gg/uDTgxyg',
-  version: 'v0.1.0b',
-  url: 'https://dbots.co',
-  guildId: '236608364333891585'
-};
